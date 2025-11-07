32ac2ac3166b284030da69935054dc18f2701d25
Jakob Olason <jakobolason@proton.me>
2025-11-07 23:00:57 +0100

    discovery works in minikube with v0.0.2

--- src/app.rs
+++ src/app.rs
@@ -4,6 +4,7 @@ use leptos_router::{
     components::{Route, Router, Routes},
     StaticSegment,
 };
+use std::cell::Cell;
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
@@ -50,12 +51,57 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let count = RwSignal::new(0);
-    let on_click = move |_| *count.write() += 1;
+    let cookies = vec![
+        "Today its up to you to create the peacefulness you long for.",
+        "A friend asks only for your time not your money.",
+        "If you refuse to accept anything but the best, you very often get it.",
+        "A smile is your passport into the hearts of others.",
+        "A good way to keep healthy is to eat more Chinese food.",
+        "Your high-minded principles spell success.",
+        "Hard work pays off in the future, laziness pays off now.",
+        "Change can hurt, but it leads a path to something better.",
+        "Enjoy the good luck a companion brings you.",
+        "People are naturally attracted to you.",
+        "Hidden in a valley beside an open stream- This will be the type of place where you will find your dream.",
+        "A chance meeting opens new doors to success and friendship.",
+        "You learn from your mistakes... You will learn a lot today.",
+        "If you have something good in your life, don’t let it go!",
+        "What ever you’re goal is in life, embrace it visualize it, and for it will be yours.",
+        "Your shoes will make you happy today.",
+        "You cannot love life until you live the life you love.",
+        "Be on the lookout for coming events; They cast their shadows beforehand.",
+        "Land is always on the mind of a flying bird.",
+        "The man or woman you desire feels the same about you.",
+        "Meeting adversity well is the source of your strength.",
+        "A dream you have will come true.",
+        "Our deeds determine us, as much as we determine our deeds.",
+        "Never give up. You’re not a failure if you don’t give up.",
+        "You will become great if you believe in yourself.",
+        "There is no greater pleasure than seeing your loved ones prosper.",
+        "You will marry your lover.",
+        "A very attractive person has a message for you.",
+        "You already know the answer to the questions lingering inside your head.",
+        "It is now, and in this world, that we must live.",
+        "You must try, or hate yourself for not trying.",
+        "You can make your own happiness."
+    ];
+    let cookie_val = RwSignal::new(cookies[0]);
+    let seed = Cell::new(12345u32);
 
+    let new_cookie_click = move |_| *cookie_val.write() = {
+        let current = seed.get();
+        let next = current.wrapping_mul(1103515245).wrapping_add(12345);
+        seed.set(next);
+
+        let random_index = (next as usize) % cookies.len();
+        cookies[random_index]
+    };
+    
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h2>Fortune cookie selector: </h2>
+        <button on:click=new_cookie_click>"get new cookie"</button>
+        <p>
+            {cookie_val}
+        </p> 
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,171 @@
+use axum::{Router, 
+    routing::get,
+    middleware::{self, Next},
+    response::Response,
+    http::Request,
+    body::Body,
+    extract::{FromRef, State},
+    Json,
+};
+use std::{
+    net::{IpAddr, Ipv4Addr}, 
+    sync::{Arc, RwLock}, 
+    time::{SystemTime, UNIX_EPOCH},
+    collections::{HashSet, HashMap},
+    env,
+};
+use leptos::{config::LeptosOptions, html::U};
+// use uuid::Uuid;
+// use tower_http::trace::{TraceLayer, ServerErrorsFailureClass};
+// use tracing::{info, error, info_span, Span};
+// use std::time::Duration;
+use tokio::{time::{sleep, Duration, interval}, net::lookup_host};
+
+fn generate_node_id() -> u32 {
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_nanos();
+    (now % 101) as u32
+}
+
+#[derive(Clone)]
+struct AppState {
+    leptos_options: LeptosOptions,
+    leader: Arc<RwLock<(u32, u32)>>,
+    ip_list: Arc<RwLock<Vec<(u32, u32)>>>,
+    node_ip: u32,
+    node_id: u32,
+    port: u16,
+}
+impl AppState {
+    fn new(leptos_options: LeptosOptions, node_ip: u32, port: u16, node_id: u32) -> Self {
+        Self {
+            leptos_options,
+            leader: Arc::new(RwLock::new((0,0))),
+            ip_list: Arc::new(RwLock::new(Vec::new())),
+            node_ip,
+            node_id,
+            port,
+        }
+    }
+}
+impl FromRef<AppState> for LeptosOptions {
+    fn from_ref(state: &AppState) -> Self {
+        state.leptos_options.clone()
+    }
+}
+
+
+async fn middleware_check_is_leader(request: Request<Body>, next: Next) -> Response {
+    println!("request to: {}", request.uri());
+    let response = next.run(request).await;
+
+    println!("response status: {}", response.status());
+    response
+}
+
+async fn discovery(service_name: &str, port: u16) -> Result<Vec<(u32, u32)>, std::io::Error> {
+    println!("Making a DNS lookup to service");
+    
+    let address = format!("{}:{}", service_name, port);
+    let addresses = lookup_host(&address).await?;
+    
+    println!("Got response from DNS");
+    
+    let mut ip_list: Vec<Ipv4Addr> = Vec::new();
+    for addr in addresses {
+        if let std::net::IpAddr::V4(ipv4) = addr.ip() {
+            ip_list.push(ipv4);
+        }
+    }
+    
+    // now get the ids of all these ips
+    let mut ip_map = HashMap::new();
+    for ip in ip_list {
+        let url = format!("http://{}:{}/node_id", ip, port);
+        let ip_addr = u32::from(ip);
+        match reqwest::get(&url).await {
+            Ok(resp) => {
+                match resp.json::<u32>().await {
+                    Ok(node_id) => {
+                        println!("Got node_id {} from {:?}", node_id, ip_addr);
+                        ip_map.insert(ip_addr, node_id);
+                    }
+                    Err(e) => eprintln!("Error parsing response from {}: {}", ip_addr, e),
+                }
+            }
+            Err(e) => eprintln!("Error with request to {}: {}", ip_addr, e),
+        } 
+    }
+    let ip_vec: Vec<(u32, u32)> = ip_map.into_iter().collect();
+    Ok(ip_vec)
+}
+
+// background heartbeat check
+async fn background_task(state: AppState) {
+    // let other nodes settle
+    let mut interval = interval(Duration::from_secs(10));
+    
+    loop {
+        interval.tick().await;
+        println!("background task for node {} running at: {:?}",state.node_id, std::time::SystemTime::now());
+        match discovery("bully-service", state.port).await {
+            Ok(ips) => {
+                println!("Found ips: {:?}", ips);
+            }
+            Err(e) => {
+                eprintln!("DNS lookup failed: {}", e);
+            }
+        }
+    }
+
+}
+
+async fn get_node_id(State(state): State<AppState>) -> Json<u32> {
+    Json(state.node_id)
+}
 
 #[cfg(feature = "ssr")]
 #[tokio::main]
 async fn main() {
-    use axum::Router;
     use leptos::logging::log;
     use leptos::prelude::*;
     use leptos_axum::{generate_route_list, LeptosRoutes};
     use fortune_cookies::app::*;
 
-    let conf = get_configuration(None).unwrap();
+    let port = env::var("WEB_PORT")
+        .ok()
+        .and_then(|p| p.parse::<u16>().ok())
+        .unwrap_or(3000);
+    
+    // Override the site_addr with environment variable
+    let mut conf = get_configuration(None).unwrap();
+    conf.leptos_options.site_addr = format!("0.0.0.0:{}", port).parse().unwrap();
+    
     let addr = conf.leptos_options.site_addr;
     let leptos_options = conf.leptos_options;
     // Generate the list of routes in your Leptos App
     let routes = generate_route_list(App);
+    let ip_addr = match addr.ip() {
+        IpAddr::V4(ipv4) => u32::from(ipv4),
+        IpAddr::V6(_) => panic!("IPv6 not supported!")
+    };
+    let state = AppState::new(leptos_options, ip_addr, port, generate_node_id());
 
     let app = Router::new()
-        .leptos_routes(&leptos_options, routes, {
-            let leptos_options = leptos_options.clone();
+        .leptos_routes(&state, routes, {
+            let leptos_options = state.leptos_options.clone();
             move || shell(leptos_options.clone())
         })
-        .fallback(leptos_axum::file_and_error_handler(shell))
-        .with_state(leptos_options);
+        .route("/status",get(get_status))
+        .route("/node_id", get(get_node_id))
+        // .layer(tracing_layer)
+        .layer(middleware::from_fn(middleware_check_is_leader))
+        .fallback(leptos_axum::file_and_error_handler::<AppState, _>(shell))
+        .with_state(state.clone());
 
+    let background_handle = tokio::spawn(background_task(state.clone()));
     // run our app with hyper
     // `axum::Server` is a re-export of `hyper::Server`
     log!("listening on http://{}", &addr);
@@ -29,6 +173,11 @@ async fn main() {
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
+    background_handle.abort();
+}
+
+async fn get_status() -> &'static str{
+    "OK"
 }
 
 #[cfg(not(feature = "ssr"))]
